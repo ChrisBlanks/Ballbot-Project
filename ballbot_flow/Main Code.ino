73759e87f8c06f195d55a13ab67842b617d76946
CBlanks <34360381+ChrisBlanks@users.noreply.github.com>
2018-03-12 22:25:04 -0400

    Updated the threshold value
    threshold = 550

--- ballbot_flow/Main Code.ino
+++ ballbot_flow/Main Code.ino
@@ -21,14 +21,14 @@ Adafruit_DCMotor *myMotor1 = AFMS.getMotor(1); //Creates motor objects from getM
 Adafruit_DCMotor *myMotor2= AFMS.getMotor(2);
 
 /* paramaters that hold sensor and control data */
-double battery,light_sens_val, threshold, final_pwm_x, final_pwm_y;
+double battery,light_sens_val, final_pwm_x, final_pwm_y;
 double final_gyro_y, final_gyro_x,final_phi_m_y, final_phi_m_x;
 double gyro_y_offset,gyro_x_offset,motor_y_offset ,motor_x_offset;
 
 int state, run_check, stop_run = 0, calibrate = 1, control = 2; //State control
 int flag_firstcycle = 1; /* boolean that is used in the control code and flow code */
 //Needs to be matched up with the control code still
-
+int threshold = 550;
 const int button_pin = 10; /* Pin that will start the calibration and run process */
 const int light_sens_pin = 9; /* Pin that will sense if the ball is present */
 
@@ -38,7 +38,6 @@ void setup() {
  
  Serial.begin(9600);
  pinMode(button_pin,INPUT);
- pinMode(light_sens_pin,INPUT);
  
  initialization(); //Runs intiliazation task and sets up initial parameters before calibration
  
@@ -54,6 +53,7 @@ void loop() {
 /* subfunctions */
 
 
+// Modular function made to abstract the process
 void bno_Setup(void){
   
   if( !bno.begin() ) /* If the sensor is not read by the arduino */
@@ -67,7 +67,7 @@ void bno_Setup(void){
   bno.setExtCrystalUse(true); /* sets an external 32.768 kHZ crystal for the clock (more accurate) */
   }
 
-  
+//Populates the necessary variables
 void initial_offset(void){
   
   sensors_event_t event;
@@ -91,7 +91,7 @@ void motors_off(void){
   myMotor2 -> setSpeed(0);
   }
 
-  
+// Code that is executed when the Arduino turns on
 void initialization(void){
   battery = read_voltage();
   motors_off();
@@ -100,7 +100,20 @@ void initialization(void){
   initial_offset();
   }
 
-  
+/* 
+ *  This function passes along the proper sensor readings to the control code and
+ *  reads the control pwm (or duty cycle) values.
+ *  
+ *  The control code is defined in the header files and C-scripts that are included
+ *  in the beginning of this code. 
+ *  
+ *  "ert_main.c" has the same function calls that are
+ *  used in this script. "ert_main.h" was created so that these functions could be called
+ *  in this script. "feed_control.c" defines the function calls used in "ert_main.c", and
+ *  "feedback_control.h" defines the structs used.
+ *  
+ */ 
+ 
 void state_est_and_control(double gyro_y_val, double gyro_x_val,double phi_angle_y,
                double phi_angle_x,real_T *pwm_y_direction, real_T *pwm_x_direction){
   //Inputs to control code
@@ -130,6 +143,7 @@ void state_est_and_control(double gyro_y_val, double gyro_x_val,double phi_angle
   
 double read_voltage(void){
   /* This sub-function is for reading the battery voltage  */
+  /* Still being developed */
   
   int batt_volt = 5;   //placeholder value
   //batt_volt = analogRead(pin_number);
@@ -139,7 +153,7 @@ double read_voltage(void){
 
 void state_check(void){
   light_sens_val = analogRead(light_sens_pin);
-  run_check = digitalRead(button_pin);
+  run_check = digitalRead(button_pin); //Checks to see if button has been pressed during this loop
   battery = read_voltage(); //Continuously check battery voltage
                             //May need to use low pass filter
   double phi_angle_y, phi_angle_x; //local variables for transfering encoder readings
@@ -156,25 +170,40 @@ void state_check(void){
   final_phi_m_y = phi_angle_y;
   final_phi_m_x = phi_angle_x;
 
-  /* If statements control state of ballbot.
+  /* Below are If statements control state of ballbot.
+   *  
    * If the button is pressed, the initial state is stop_run, 
    * and the light sensor's value is low (due to blockage by the ball) 
    * then the sensors will be calibrated again
    *  
+   *  Note about light sensor:
+   *  
+   *  The light sensor reads a new value every loop. 
+   *  These values are representative of the voltage generated
+   *  by the amount of light that is being received by the 
+   *  phototransistor in the light sensor. The span of the values
+   *  is from 0 to 1023 (10 bit).
+   *  
+   *  Normally, the actual readings in an envirnoment with typical 
+   *  amounts of ambient light will range from 450 to 700. 
+   *  For this project, a reading of 450 indicates that the ball is
+   *  in place. A value over 550 usually indicates that the ball has been
+   *  lost. The ballbot will only operate under a value of 550. For different
+   *  environments, this threshold value may need to be adjusted.
    *  
    */
    
   if(run_check == HIGH && light_sens_val <= threshold && state == stop_run){
     sensor_calibrate();
-    state = calibrate ;
+    state = calibrate ; //Allows ballbot to enter the next loop
     }
   if(state == calibrate && light_sens_val <= threshold){
-    balance_and_control();
-    state = control;
+    balance_and_control(); //Function that initiates the control portion of the code
+    state = control; //Allows passage into the next loop
     }
   if(state == control && light_sens_val >= threshold){
     state = stop_run;
-    motors_off();
+    motors_off(); //Ballbot is turned off if ball is lost
     }
 
   }
@@ -205,19 +234,19 @@ void sensor_calibrate(void){
   encoder_read_x(&e_angle_x);
 
   
-  motor_y_offset = -1.0 * e_angle_y; // Inverts readings
+  motor_y_offset = -1.0 * e_angle_y; // Inverts readings 
   motor_x_offset = -1.0 * e_angle_x;
   }
 
 
-void encoder_read_x(double *x_read){ //function for reading X encoder value and getting an
+void encoder_read_x(double *x_read){ // future function for reading X encoder value and getting an angle
    
   *x_read= 1.0;
   
   }
 
 
-void encoder_read_y(double *y_read){ //function for reading Y encoder value and getting an angle
+void encoder_read_y(double *y_read){ // future function for reading Y encoder value and getting an angle
   
   *y_read = 1.0;
   
