12e8622c2449eb8724a04a7db0abe612a8dd2bab
CBlanks <34360381+ChrisBlanks@users.noreply.github.com>
2018-04-28 22:44:11 -0400

    Re-uploaded in order to show progression
    Definitely won't compile. haha

---
+++ Previous versions/3rd version/ert_main.h
@@ -0,0 +1,4 @@
+#ifndef ert_main_h
+#define ert_main_h
+void rt_OneStep(void);
+#endif
---
+++ Previous versions/3rd version/feedback_control_types.h
@@ -0,0 +1,28 @@
+/*
+ * File: feedback_control_types.h
+ *
+ * Code generated for Simulink model 'feedback_control'.
+ *
+ * Model version                  : 1.49
+ * Simulink Coder version         : 8.12 (R2017a) 16-Feb-2017
+ * C/C++ source code generated on : Tue Feb 27 16:41:58 2018
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_feedback_control_types_h_
+#define RTW_HEADER_feedback_control_types_h_
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_feedback_control RT_MODEL_feedback_control;
+
+#endif                                 /* RTW_HEADER_feedback_control_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
